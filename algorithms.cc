3c1b05ba1c15f5df7069bf10c63f5f1788d38bfc
kmmakinaci <kmmakinaci@gmail.com>
2023-12-29 14:08:42 +0100

    Accumulate : refactor with algortihms

--- algorithms.cc
+++ algorithms.cc
@@ -2,6 +2,7 @@
 This code is from the https://www.youtube.com/watch?v=eJCA2fynzME&t=1443s
 Back to Basics: (Range) Algorithms in C++ - Klaus Iglberger - CppCon 2023
 */
+#include <stdio.h>
 #include <vector>
 #include <algorithm>
 
@@ -24,12 +25,18 @@ int partition()
 int accumulate()
 {
     std::vector<Person> table = /*...*/;
+    
+    // Prefer to keep things immutable.
+    // Anti pattern : Initialize then modify
 
-    int total_age = 0;
-    for( auto const& person : table) {
-        total_age += person.age;
-    }
-    std::cout << "Total age =" << total_age << '\n';
+    auto const age =
+        std::accumulate( std::begin(table), std::end(table), 0,
+        [](int age, Person const& p )
+        {
+            return age + p.age;
+        } );
+
+    std::cout << "Total age = " << age << '\n';
 
     // ...
 }
