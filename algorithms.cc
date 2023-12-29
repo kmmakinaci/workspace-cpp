3eba0a4767a5783dfc13c093ac6c811149d7748a
kmmakinaci <kmmakinaci@gmail.com>
2023-12-29 13:52:22 +0100

    Partition : keep relative order

--- algorithms.cc
+++ algorithms.cc
@@ -1,13 +1,22 @@
+/*DISCLAIMER
+This code is from the https://www.youtube.com/watch?v=eJCA2fynzME&t=1443s
+Back to Basics: (Range) Algorithms in C++ - Klaus Iglberger - CppCon 2023
+*/
 #include <vector>
+#include <algorithm>
 
 //partition
-int partition(){
+int partition()
+{
 
     std::vector<Person> table = /* ...*/;
 
-    std::ranges::partition(table, [](Person const& p))
+    auto const pos =
+    std::ranges::stable_partition(table, [](Person const& p))
     {
         return isChild(p);
         //return isSeniorCitizen(p);
     }
+
+    std::ranges::rotate(table, pos);
 }
