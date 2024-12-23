a7246f2069ba00b4249659db28338c0c87e041d4
unknown <kmmakinaci@gmail.com>
2024-12-23 15:43:05 +0300

    Refactor and add documentation
    Enhance implementation for more readability and add documentation.

--- Aggregation.cpp
+++ Aggregation.cpp
@@ -1,48 +1,87 @@
+/*
+Let us try to illustrate the implementation of the aggregation type of relation between 2 classes;
+ Person and Address
 
-//Let us try to illustrate the implementation of the aggregation type of relation between 2 classes Person and address using a C++ program.
+Aggregation is a type of association where one class is a part of another class. 
+It represents a "has-a" relationship. In this example, a Person "has an" Address.
 
+UML Class Diagram:
+------------------
++----------------+        +----------------+
+|    Person      |        |    Address     |
++----------------+        +----------------+
+| - name: string |<>------| - houseNo: int |
+| - address:     |        | - colony: string|
+|   shared_ptr   |        | - city: string |
+|   <Address>    |        | - state: string|
++----------------+        +----------------+
+| + getName():   |        | + getHouseNo():|
+|   string       |        |   int          |
+| + getAddress():|        | + getColony(): |
+|   shared_ptr   |        |   string       |
+|   <Address>    |        | + getCity():   |
+| + setAddress(  |        |   string       |
+|   shared_ptr   |        | + getState():  |
+|   <Address>):  |        |   string       |
+|   void         |        +----------------+
+| + display():   |
+|   void         |
++----------------+
+*/
 
 #include <iostream>
-#include<string.h>
+#include <memory>
+#include <string>
 
+using namespace std;
 
 class Address {
 public:
     int houseNo;
-    string colony,city, state;
+    string colony, city, state;
     
-    Address(int hno, string colony, string city, string state)
-    {
-        this->houseNo = hno;
-        this->colony = colony;    
-        this->city = city;
-        this->state = state;
-    }
+    Address(int hno, const string& colony, const string& city, const string& state)
+        : houseNo(hno), colony(colony), city(city), state(state) {}
+
+    int getHouseNo() const { return houseNo; }
+    string getColony() const { return colony; }
+    string getCity() const { return city; }
+    string getState() const { return state; }
 };
 
 class Person {
 private:
-    Address* address;
-public:
     string name;
-    Person(string name, Address* address)
-    {
-        this->name = name;
-        this->address = address;
+    shared_ptr<Address> address;
+public:
+    Person(const string& name, shared_ptr<Address> address)
+        : name(name), address(address) {}
+
+    string getName() const { return name; }
+    shared_ptr<Address> getAddress() const { return address; }
+
+    void setAddress(shared_ptr<Address> newAddress) {
+        address = newAddress;
     }
 
-    void display()
-    {
-        cout<< name<< " "<< " "<< address->houseNo<<" "<<address-> colony<<" " <<address->city<< " "<<address->state<<endl;
+    void display() const {
+        cout << name << " " << address->houseNo << " " << address->colony << " " << address->city << " " << address->state << endl;
     }
 };
 
-int main(void) {
-    Address add1= Address(868 ,"Mahavir Colony","Jahagirpuri","New Delhi");
-    Person p1 = Person("Raj",&add1);
-    Person p2 = Person("Seema",&add1);
+int main() {
+    auto add1 = make_shared<Address>(868, "Mahavir Colony", "Jahagirpuri", "New Delhi");
+    auto add2 = make_shared<Address>(123, "Green Park", "South Delhi", "New Delhi");
+
+    Person p1("Raj", add1);
+    Person p2("Seema", add1);
+
     p1.display();
     p2.display();
+
+    // Update address for p1
+    p1.setAddress(add2);
+    p1.display();
     
     return 0;
 }
