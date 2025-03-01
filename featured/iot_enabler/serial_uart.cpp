ac730e965b2c39abab75a7105f5b6b4c353d98ac
unknown <kmmakinaci@gmail.com>
2025-03-01 18:56:47 +0100

    Refactor serial UART handler
    Make this code non c-style and more C++ style.
    Think abput creation of a class for UART handling. Apply RAII,
    move semantics and other C++ features.

--- featured/iot_enabler/serial_uart.cpp
+++ featured/iot_enabler/serial_uart.cpp
@@ -3,39 +3,97 @@
 #include <termios.h>
 #include <unistd.h>
 #include <cstring>
+#include <memory>
+#include <string>
+
+class SerialPort {
+public: 
+    
+    SerialPort(const std::string& port, int baudrate){
+        serial_fd = open(port.c_str(), O_RDWR | O_NOCTTY);
+        if (serial_fd < 0) {
+            throw std::runtime_error("Failed to open serial port!");
+        }
+        configureTTY(serial_fd, baudrate);
+    }
+    ~SerialPort() {
+        if (serial_fd >= 0) {
+            close(serial_fd);
+        }
+    }
+
+    //Move constructor
+    SerialPort(SerialPort&& other) noexcept : serial_fd(other.serial_fd) {
+        other.serial_fd = -1; //prevent double close
+    };
+
+    //Move assignment
+    SerialPort& operator=(SerialPort&& other) noexcept{
+        if (this != &other){
+            if(serial_fd >= 0)
+                close(serial_fd);
+            serial_fd = other.serial_fd;
+            other.serial_fd = -1;
+        }
+        return *this;
+    }
+
+    //Delete copy constructor and copy assignment - serial fd cannot copiable
+    SerialPort(const SerialPort&) = delete;
+    SerialPort& operator=(const SerialPort&) = delete;
+
+    void write(const std::string& data) {
+        ::write(serial_fd, data.c_str(), data.size());
+    }
+
+    std::string read() {
+        char buffer[256];
+        int n = ::read(serial_fd, buffer, sizeof(buffer));
+        if n >= 0 {
+            return std::string(buffer, n);
+        }
+        return {};
+    }   
+private:
+    int serial_fd;
+
+    configureTTY(int serial_fd, int baudrate){
+        struct termios tty;
+        memset(&tty, 0, sizeof(tty));
+        
+        if(tcgetattr(serial_fd, &tty) != 0){
+            throw std::runtime_error("Failed to get serial port attributes!");
+        }
+
+        cfsetospeed(&tty, baudrate);
+        cfsetispeed(&tty, baudrate);
+        tty.c_cflag |= (CLOCAL | CREAD);
+        tty.c_cflag &= ~CSIZE;
+        tty.c_cflag |= CS8;
+        tty.c_cflag &= ~PARENB;
+        tty.c_cflag &= ~CSTOPB;
+
+        if(tcsetattr(serial_fd, TCSANOW, &tty) != 0){
+            throw std::runtime_error("Failed to set serial port attributes!");
+        }
+    }
+};
+
+#define SERIAL_PORT "/dev/ttyUSB0"
+#define BAUD_RATE B115200
 
 int main() {
-    int serial_fd = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
-    if (serial_fd < 0) {
-        std::cerr << "Failed to open serial port!" << std::endl;
+    try {
+        SerialPort com_serial(SERIAL_PORT, BAUD_RATE);
+        com_serial.write("AT\r\n");
+        std::string response = com_serial.read();
+        if (!response.empty()) {
+            std::cout << "Received: " << response << std::endl;
+        }   
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
         return -1;
     }
 
-    struct termios tty;
-    memset(&tty, 0, sizeof(tty));
-    tcgetattr(serial_fd, &tty);
-
-    cfsetospeed(&tty, B115200);
-    cfsetispeed(&tty, B115200);
-
-    tty.c_cflag |= (CLOCAL | CREAD);
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-
-    tcsetattr(serial_fd, TCSANOW, &tty);
-
-    char at_command[] = "AT\r\n";
-    write(serial_fd, at_command, sizeof(at_command));
-
-    char buffer[256];
-    int n = read(serial_fd, buffer, sizeof(buffer));
-    if(n>0) {
-        buffer[n] = '\0';
-        std::cout << "Received: " << buffer << std::endl;
-    }
-
-    close(serial_fd);
     return 0;
 }
